cffacac78795824ef626df3db47539d69c907cef
Andrea Bucaletti <erbuka@hotmail.com>
2023-03-02 13:22:30 +0100

    --

--- bin/index.js
+++ bin/index.js
@@ -13,42 +13,44 @@ const targetSize = parseInt(argv.size) || 1024;
 const recursive = argv.recursive || false;
 
 const listFiles = function (dir) {
-    let result = []
-    for (let f of fs.readdirSync(path.resolve(dir))) {
-        let file = path.resolve(dir, f);
-        if (fs.lstatSync(file).isFile() && fileExt.some(ext => file.endsWith(ext)))
-            result.push(file);
-    }
-    return result;
+	let result = []
+	for (let f of fs.readdirSync(path.resolve(dir))) {
+		let file = path.resolve(dir, f);
+		if (fs.lstatSync(file).isFile() && fileExt.some(ext => file.endsWith(ext)))
+			result.push(file);
+	}
+	return result;
 }
 
 const listFolders = function (dir) {
-    let result = [path.resolve(dir)];
-    for (let f of fs.readdirSync(path.resolve(dir))) {
-        let file = path.resolve(dir, f);
-        if (fs.lstatSync(file).isDirectory())
-            result.push(...listFolders(file));
-    }
-    return result;
+	let result = [path.resolve(dir)];
+	for (let f of fs.readdirSync(path.resolve(dir))) {
+		let file = path.resolve(dir, f);
+		if (fs.lstatSync(file).isDirectory())
+			result.push(...listFolders(file));
+	}
+	return result;
 }
 
 const processImages = async function (files) {
 
-    await Promise.all(files.map(async (f) => {
-        let val = await jimp.read(f);
+	await Promise.all(files.map(async (f) => {
+		let val = await jimp.read(f);
 
-        let [w, h] = [val.bitmap.width, val.bitmap.height];
+		let [w, h] = [val.bitmap.width, val.bitmap.height];
 
-        let nw = w >= h ? targetSize : w / h * targetSize;
-        let nh = w >= h ? targetSize * h / w : targetSize;
+		let nw = w >= h ? targetSize : w / h * targetSize;
+		let nh = w >= h ? targetSize * h / w : targetSize;
 
-        console.log(`Resize ${colors.yellow(path.basename(f))}: [${w} x ${h}] => [${nw} x ${nh}]`);
-        return new Promise((resolve) => val.resize(nw, nh).write(f, () => resolve()));
-    }));
+		if (w >= nw || h >= nh) {
+			console.log(`Resize ${colors.yellow(path.basename(f))}: [${w} x ${h}] => [${nw} x ${nh}]`);
+			return new Promise((resolve) => val.resize(nw, nh).write(f, () => resolve()));
+		}
+	}));
 
 }
 
 const folders = recursive ? listFolders(targetDir) : [path.resolve(targetDir)];
 
 for (let dir of folders)
-    processImages(listFiles(dir));
+	processImages(listFiles(dir));
